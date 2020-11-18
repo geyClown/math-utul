0d23648cb3b2b7613696ca3cb2fad761a483e130
geyClown <hosukikang@gmail.com>
2020-11-18 10:25:45 +0700

    dequy

--- src/bao/utul/MathUtility.java
+++ src/bao/utul/MathUtility.java
@@ -23,18 +23,32 @@ public class MathUtility {
     // giai thua tang cuc nhanh, 21 long ko chua noi
     //bai tinh giai thua tu 0..20
     //21 tro len ko tinh
-    public static long getFactorial(int n) {
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("n must be 0 to 20");
+//
+//        }
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//        long result = 1;
+//        for (int i = 2; i <= n; i++) {
+//            result *= i;
+//        }
+//        return result;
+//    }
+    //ko chơi bình thuognwf chơi đệ quy
+    public static long getFactorial(int n){
+        //đệ quy là gọi lại mình với quy mô nhỏ hơn
+        //phải có dừng vì không thì lặp vô tận
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("n must be 0 to 20");
 
         }
-        if (n == 0 || n == 1) {
+              if (n == 0 || n == 1) {
             return 1;
         }
-        long result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        return result;
+              return n*getFactorial(n-1);
+              //di cho toi khi ve 1 thì thoi
     }
 }
